16b08d48c34d74889c50cf42a5b701e7e4aa5105
SadboiTay <tg.hakes@gmail.com>
2021-11-20 11:44:43 -0700

    create user model and formate date util

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'You must create a username!',
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'You must provide a valid email addres!',
+            trim: true
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }
+)
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
